b95b6aaab71ac85469aed06c0ccf5a38dbf40b33
Bharat Kumar <k.bharatkumar815@gmail.com>
2025-11-05 21:59:00 +0530

    finished

---
+++ src/config/Quote.tsx
@@ -0,0 +1,14 @@
+export const quotes: { quote: string; author: string }[] = [
+  {
+    quote: "Simplicity is the soul of efficiency.",
+    author: "Austin Freeman",
+  },
+  {
+    quote: "Programs must be written for people to read, and only incidentally for machines to execute.",
+    author: "Harold Abelson",
+  },
+  {
+    quote: "First, solve the problem. Then, write the code.",
+    author: "John Johnson",
+  },
+];
